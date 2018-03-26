fc19cb5595c4b133fa5729cff626acff1d1a2f6b
Andrew Che <andrew@codeninja55.me>
2018-03-27 01:05:00 +1100

    Minor code format style changes

--- assignment02/CardSet.h
+++ assignment02/CardSet.h
@@ -16,13 +16,13 @@ class CardSet
 		bool IsEmpty() const;
 		void Shuffle();
 		int Deal();
-		void Deal(int,CardSet&,CardSet&);
-		void Deal(int,CardSet&,CardSet&,CardSet&,CardSet&);
+		void Deal(int, CardSet&, CardSet&);
+		void Deal(int, CardSet&, CardSet&, CardSet&, CardSet&);
 		void AddCard(int);
 		void MergeShuffle(CardSet&);
 		void Print() const;
 	private:
-		int* Card;
+		int *Card;
 		int nCards;
 		void PrintCard(int c) const;
 };
