c055bf831c91333d73089d5845c6308909c89612
Andrew Che <andrew@codeninja55.me>
2018-03-31 01:51:53 +1100

    Changed testing

--- assignment02/main.cpp
+++ assignment02/main.cpp
@@ -40,16 +40,17 @@ int main()
 	cout << "Printout of CardSet1:" << endl;
 	CardSet1.Print();
 	cout << endl;
-    if(debug) cout << "=================================================="<<endl;
+    if(debug) cout << "=================================================================="<<endl;
 
-    /*cout << "Test 3: Normal Shuffle: "<< endl;
-    if(debug) cout << "=================================================="<<endl;
+    cout << "Test 3: Normal Shuffle: "<< endl;
+    if(debug) cout << "=================================================================="<<endl;
     CardSet1.Shuffle();
     cout << "Printout of shuffled CardSet1:" << endl;
     CardSet1.Print();
     cout << endl;
-    if(debug) cout << "=================================================="<<endl;*/
+    if(debug) cout << "=================================================================="<<endl;
 
+    // TODO: Need to debug this
     cout << "Test 4: Merge Shuffle: "<< endl;
     if(debug) cout << "=================================================="<<endl;
     cout << "Printout of CardSet1:" << endl;
@@ -63,34 +64,38 @@ int main()
     cout << "CardSet1 Size: " << CardSet1.Size() << endl;
     CardSet1.Print();
     cout << endl;
-    if(debug) cout << "=================================================="<<endl;
-
-/*
+    if(debug) cout << "=================================================================="<<endl;
 
 	cout << "Test 5: Dealing 3 Cards from CardSet1 into CardSet3 and CardSet4: " << endl;
-	CardSet1.Deal(3,CardSet3,CardSet4);
-	cout << "Printout of CardSet1:" << endl;
+    if(debug) cout << "=================================================================="<<endl;
+    cout << "Printout of CardSet1 (before):" << endl;
+    CardSet1.Print();
+    CardSet1.Deal(3, CardSet3, CardSet4);
+    cout << "Printout of CardSet1:" << endl;
 	CardSet1.Print();
 	cout << "Printout of CardSet3:" << endl;
 	CardSet3.Print();
 	cout << "Printout of CardSet4:" << endl;
 	CardSet4.Print();
 	cout << endl;
+    if(debug) cout << "====================================================================="<<endl;
 
-	cout << "Test 6: Dealing 3 Cards from CardSet1 into CardSet 3, 4, 5 & 6: " << endl;
-	CardSet1.Deal(3,CardSet3,CardSet4,CardSet5,CardSet6);
-	cout << "Printout of CardSet1:" << endl;
-	CardSet1.Print();
-	cout << "Printout of CardSet3:" << endl;
-	CardSet3.Print();
-	cout << "Printout of CardSet4:" << endl;
-	CardSet4.Print();
-	cout << "Printout of CardSet5:" << endl;
-	CardSet5.Print();
-	cout << "Printout of CardSet6:" << endl;
-	CardSet6.Print();
-	cout << endl;
-*/
-	cout << ". . . END OF TESTS"<< endl;
+    /*cout << "Test 6: Dealing 3 Cards from CardSet1 into CardSet 3, 4, 5 & 6: " << endl;
+    if(debug) cout << "====================================================================="<<endl;
+    CardSet1.Deal(3,CardSet3,CardSet4,CardSet5,CardSet6);
+    cout << "Printout of CardSet1:" << endl;
+    CardSet1.Print();
+    cout << "Printout of CardSet3:" << endl;
+    CardSet3.Print();
+    cout << "Printout of CardSet4:" << endl;
+    CardSet4.Print();
+    cout << "Printout of CardSet5:" << endl;
+    CardSet5.Print();
+    cout << "Printout of CardSet6:" << endl;
+    CardSet6.Print();*/
+
+    cout << endl;
+    if(debug) cout << "====================================================================="<<endl;
+    cout << ". . . END OF TESTS"<< endl;
 	return 0;
 }
