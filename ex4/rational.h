33105fbc6e07a7301c2531fba6f4fddba2430d1d
Andrew Che <andrew@codeninja55.me>
2018-05-19 09:57:06 +1000

    Completed ex4 Rational

--- ex4/rational.h
+++ ex4/rational.h
@@ -1,28 +1,33 @@
 /*************************************************************************
  * CSCI251 Ex4 - rational.h: Declaration of Rational class
- * <Student Name>, <login>, <date completed>
+ * Dinh Che, dbac496, 15.05.18
  *************************************************************************/
 #ifndef _RATIONAL_H_
 #define _RATIONAL_H_
 #include <iostream>
 using namespace std;
 
-//void printRationalAsFloating(const Rational &r);
-
 class Rational {
-  public:
-	Rational();  // default constructor
-	Rational(int, int); //std (initialisation) constructor
-	Rational(const Rational&); //copy constructor
-	Rational addition(const Rational&);
-	Rational subtraction(const Rational&);
-	Rational multiplication(const Rational&);
-	Rational division(Rational &);
-	void printRational();
+    public:
+	    Rational();  // default constructor
+	    Rational(int, int); // std (initialisation) constructor
+	    Rational(const Rational&); // copy constructor
+        ~Rational(); // destructor
+        Rational(int); // testing zero denominator constructor
+	    Rational addition(const Rational&);
+	    Rational subtraction(const Rational&);
+	    Rational multiplication(const Rational&);
+	    Rational division(Rational&);
+        void printRational();
+        static string divideByZeroException;
+        static string zeroDenominatorException;
+        static int getCount();
+        friend void printRationalAsFloating(const Rational &r);
 
-  private:
-	int numerator;
-	int denominator;
+    private:
+        static int count;
+        int numerator;
+	    int denominator;
 };
 
 #endif
